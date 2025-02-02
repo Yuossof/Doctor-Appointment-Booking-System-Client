371ddb1aa6c18e215e5201036baa2ea190c296ac
Yuossof <youssofwr@gmail.com>
2025-02-03 00:44:50 +0200

    add hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
-<<<<<<< HEAD
 import Hero from "@/components/hero/Hero";
-export default function Home() {
-  return (
-    <div>
-      <Hero />
-=======
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
->>>>>>> 6ae4682e4646c10bfad428136630056986d91472
+      <Hero />
     </div>
   );
 }
