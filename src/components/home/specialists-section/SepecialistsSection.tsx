10b787fd43861963eae86525ea99504f0d19830d
Yuossof <youssofwr@gmail.com>
2025-02-03 22:49:35 +0200

    working on specialists section

--- src/components/home/specialists-section/SepecialistsSection.tsx
+++ src/components/home/specialists-section/SepecialistsSection.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
-
+import React from "react";
+import Slider from "react-slick";
 const SepecialistsSection = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  };
   return (
-    <div className='!w-full bg-gray-500 h-5'>SepecialistsSection</div>
+    <Slider {...settings}>
+      <div>
+        <h3>1</h3>
+      </div>
+      <div>
+        <h3>2</h3>
+      </div>
+      <div>
+        <h3>3</h3>
+      </div>
+      <div>
+        <h3>4</h3>
+      </div>
+      <div>
+        <h3>5</h3>
+      </div>
+      <div>
+        <h3>6</h3>
+      </div>
+    </Slider>
   )
 }
 
