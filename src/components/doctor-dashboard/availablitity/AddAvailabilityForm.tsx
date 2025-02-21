f1ac2d891786fcb562bbb7ce567d2ff6bae99a96
Yuossof <youssofwr@gmail.com>
2025-02-21 23:20:10 +0200

    add days section to AddAvailabilityForm

--- src/components/doctor-dashboard/availablitity/AddAvailabilityForm.tsx
+++ src/components/doctor-dashboard/availablitity/AddAvailabilityForm.tsx
@@ -6,25 +6,36 @@ import { Label } from "@/components/ui/label"
 import { Card, CardContent } from "@/components/ui/card"
 
 export function AddAvailabilityForm() {
-    const [date, setDate] = useState("")
+    const [selectedDay, setSelectedDay] = useState<string | null>(null);
     const [startTime, setStartTime] = useState("")
     const [endTime, setEndTime] = useState("")
 
-
+    const days = [
+        { id: crypto.randomUUID(), day: "Monday" },
+        { id: crypto.randomUUID(), day: "Tuesday" },
+        { id: crypto.randomUUID(), day: "Wednesday" },
+        { id: crypto.randomUUID(), day: "Thursday" },
+        { id: crypto.randomUUID(), day: "Friday" },
+        { id: crypto.randomUUID(), day: "Saturday" },
+        { id: crypto.randomUUID(), day: "Sunday" },
+    ]
     return (
         <Card className="bg-slate-800 pt-4 shadow-lg border-[1px] border-gray-700">
             <CardContent className="">
                 <form onSubmit={() => { }} className="space-y-5">
                     <div className="space-y-2">
                         <Label htmlFor="date" className="text-gray-400">Date</Label>
-                        <input
-                            id="date"
-                            type="date"
-                            className="bg-transparent text-gray-300 border-gray-600 w-full py-2 px-3 outline-none border-[1px] rounded-md"
-                            value={date}
-                            onChange={(e) => setDate(e.target.value)}
-                            required
-                        />
+                        <div className="w-full flex items-center gap-3">
+                            {days.map((day) => (
+                                <div
+                                    onClick={() => setSelectedDay(day.day)}
+                                    key={day.id}
+                                    className={` py-2 px-4 rounded-md text-gray-400 cursor-pointer 
+                                                ${selectedDay === day.day ? "border-blue-500 border-2" : "border-slate-600 border-[1px]"}`}>
+                                    {day.day}
+                                </div>
+                            ))}
+                        </div>
                     </div>
                     <div className="grid grid-cols-2 gap-4">
                         <div className="space-y-2">
@@ -59,3 +70,4 @@ export function AddAvailabilityForm() {
     )
 }
 
+// ليه بيعمل تحديد للكل
